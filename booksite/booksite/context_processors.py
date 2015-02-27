981cab938bfd111467f3b371fe3719e569a44358
Ronald Bai <ouyanghongyu@gmail.com>
2015-02-27 20:38:55 +0800

    Add google analytics settings.

--- booksite/booksite/context_processors.py
+++ booksite/booksite/context_processors.py
@@ -5,8 +5,8 @@ def analystics(request):
     context_extras = {}
     if hasattr(settings, 'BAIDUTONGJI_ID'):
         context_extras['BAIDUTONGJI_ID'] = settings.BAIDUTONGJI_ID
-    # if hasattr(settings, 'GA_ID'):
-    #     context_extras['GA_ID'] = settings.BAIDUTONGJI_ID
+    if hasattr(settings, 'GA_ID'):
+        context_extras['GA_ID'] = settings.BAIDUTONGJI_ID
     # if hasattr(settings, 'ZHANZHANGTONGJI_ID'):
     #     context_extras['ZHANZHANGTONGJI_ID'] = settings.BAIDUTONGJI_ID
     return context_extras
