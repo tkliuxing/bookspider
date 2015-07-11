edeeac0215d91d8d89017b8dac9fa42259c6b9ce
Ronald White <ouyanghongyu@gmail.com>
2015-07-11 19:36:31 +0800

    修正gunicorn配置文件错误

--- booksite/gunicorn.conf.py
+++ booksite/gunicorn.conf.py
@@ -10,6 +10,6 @@ pidfile = '/tmp/book.gunicorn.pid'
 #errorlog = '/tmp/gunicorn.error.log'
 
 try:
-    from local_gunicorn.conf.py import *
+    from local_gunicorn import *
 except ImportError:
-    raise ImportError('应当添加与gunicorn.conf.py同级别目录下的local_gunicorn.conf.py文件')
+    raise ImportError('应当添加与gunicorn.conf.py同级别目录下的local_gunicorn.py文件')
